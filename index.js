0243d4565c7b99d8dea8a0dfc00c8b43bb14eafd
Helen Liutongco <bellpickle@gmail.com>
2017-10-25 03:29:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 var kittens=["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.pop(name);
+  kittens.shift(name);
   return kittens;
 }
